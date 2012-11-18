abb3f06bc43ba66106f0957f3a6359794e13c646
Kenyon Haliwell <kenyon.jh@gmail.com>
2012-11-17 23:15:34 -0800

    Fixed issues with /applications/example.com/* not showing in Git

---
+++ applications/example.com/view/main.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+    <head>
+        <title>{title}</title>
+        <link type="text/css" rel="stylesheet" href="/webContent/fetch/main.css" />
+    </head>
+    <body>
+        <h1>{helloWorld}</h1>
+    </body>
+</html>
